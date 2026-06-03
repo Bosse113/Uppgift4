650e03bdfa1501e11287ec5d0793d1064dad47fc
Bosse113 <49038474+Bosse113@users.noreply.github.com>
2026-06-03 12:02:41 +0200

    Added Class appliance

--- Uppgift4/Appliance.cs
+++ Uppgift4/Appliance.cs
@@ -12,34 +12,43 @@ namespace Uppgift4
 //kunna spara Room
 //kunna hålla koll på IsOn
 //ha metoder som kan override:as i child classes
-        public string Brand { get; }
-        public string Room { get; }
+        public string? Brand { get; }
+        public string? Room { get; }
         public bool IsOn { get; protected set; }
         public Appliance(string brand, string room)
-        { // TODO: 
-            // Sätt Brand, Room och IsOn.
+        {
+            Brand = brand;
+            Room = room;
+            IsOn = false;
         }
-          public virtual string GetInfo() 
-        { 
-            // TODO: 
+        public virtual string GetInfo() 
+        {
+            //
             // Returnera en generell text om apparaten. 
             // Exempel: "LG in Laundry room"
+            return $"{Brand} in {Room} room.";
             throw new NotImplementedException(); 
         }
             public virtual void TurnOn() 
-        { 
-            // TODO: 
+        {
+            // 
             // Sätt IsOn till true. 
             // Skriv ut ett generellt startmeddelande.
+            IsOn = true;
+            Console.WriteLine($"{Brand} is now turned on.");
             throw new NotImplementedException(); 
         } 
-        public virtual void TurnOff() { 
-            // TODO: // Sätt IsOn till false. 
+        public virtual void TurnOff() {
+            // 
+            // Sätt IsOn till false. 
             // Skriv ut ett generellt stoppmeddelande.
+            IsOn = false;
+            Console.WriteLine($"{Brand} is now turned off.");
             throw new NotImplementedException(); } 
-        public virtual double GetDailyEnergyUsage() { 
-            // TODO: 
+        public virtual double GetDailyEnergyUsage() {
+            // 
             // Returnera 0 som standardvärde.
+            return 0;
             throw new NotImplementedException();
             //
             //
