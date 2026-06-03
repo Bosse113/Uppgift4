a20520f8a5129835532670e7823cb9232504b172
Bosse113 <49038474+Bosse113@users.noreply.github.com>
2026-06-03 09:18:32 +0200

    Answered questions

--- Uppgift4/Program.cs
+++ Uppgift4/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using System.Reflection;
+
 namespace Uppgift4
 {
     internal class Program
@@ -82,13 +84,23 @@ namespace Uppgift4
                     if (device is RobotVacuum robotVacuum)
                     {
                         robotVacuum.PrintCleaningEnergy();
-                        
+
                     }
                 }
                 // TODO: 
                 // 1. Kontrollera vilken typ device är. 
                 // 2. Casta till rätt typ. 
                 // 3. Anropa rätt energimetod. }
+                //1.Varför behövde du kontrollera vilken typ varje objekt hade?
+                //    För att veta vilken metod som ska anropas
+                //2.Vad händer om du lägger till en ny klass CoffeeMachine ?
+                //   Då måste jag skapa nya metoder och nya IF-satser.
+                //3.Vilka metoder måste du ändra om du lägger till CoffeeMachine?
+                //   Start/Stop,RunMorningRoutine och ReportAllEnergy
+                //4.Vad är problemet med att listan är List<object> ?
+                //   Programmet vet inte om object är Oven,RobotVacuum m.m.
+                //5.Vad händer om du råkar glömma en apparattyp i ReportAllEnergy()?
+                //   DÅ får vi ingen rapport på den apparattypen.
             }
         }
     }
